dff5bb73c21bdbc15c5a6551103b48102965f4d9
ArthurBrockelman <ArthurBrockelman@gmail.com>
2022-08-03 09:20:42 -0400

    module5 again

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,18 +3,12 @@ import Quiz from './Quiz';
 
 class QuizQuestionButton extends Component {
 
-    handleClick(buttonText) {
-
-        this.props.clickHandler(this.props.button_text)
-        if(buttonText === this.props.quiz_question.answer) {
-            this.props.showNextQuestionHandler()
-        }
-    }
+    
     render() {
 
         return (
             <li>
-                <button clickHandler={this.handleClick.bind(this)} >{this.props.button_text}</button>
+                <button onClick={this.handleClick.bind(this)} >{this.props.button_text}</button>
             </li>
         )
 
